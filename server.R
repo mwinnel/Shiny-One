eac0e1c73704ba5502e897a85af50c3dcf8f9676
mwinnel <melissa.winnel@griffithuni.edu.au>
2015-04-08 13:58:46 +1000

    deleted comments
    deleted a few commented out lines

--- server.R
+++ server.R
@@ -22,18 +22,6 @@ event.info <- dbGetQuery(con, "SELECT * FROM events.event_fingerprint_info")
 event.info[event.info$event_id == "Event 10",]
 
 
-#Cond1 <- read.table("data/dataset_Cond1.dat",col.names=c("Date", "Time", "Cond", "MINUTES"))
-#Cond2 <- read.table("data/dataset_Cond2.dat",col.names=c("Date", "Time", "Cond", "MINUTES"))
-#pH1 <- read.table("data/dataset_pH1.dat",col.names=c("Date", "Time", "pH", "MINUTES"))
-#pH2 <- read.table("data/dataset_pH2.dat",col.names=c("Date", "Time", "pH", "MINUTES"))
-#TempA <- read.table("data/dataset_TempA1.dat",col.names=c("Date", "Time", "TempA", "MINUTES"))
-#TempC1 <- read.table("data/dataset_TempC1.dat",col.names=c("Date", "Time", "TempC", "MINUTES"))
-#TempC2 <- read.table("data/dataset_TempC2.dat",col.names=c("Date", "Time", "TempC", "MINUTES"))
-#TurbA1 <- read.table("data/dataset_TurbA1.dat",col.names=c("Date", "Time", "TurbA", "MINUTES"))
-#TurbA2 <- read.table("data/dataset_TurbA2.dat",col.names=c("Date", "Time", "TurbA", "MINUTES"))
-#TurbS2 <- read.table("data/dataset_TurbS2.dat",col.names=c("Date", "Time", "TurbS", "MINUTES"))
-#TurbS1 <- read.table("data/dataset_TurbS1.dat",col.names=c("Date", "Time", "TurbS", "MINUTES"))
-
 shinyServer(
   
   function(input, output, session) {
